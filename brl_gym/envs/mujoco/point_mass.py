d17cce084b225c8236ce2a0a86fb23f386100ec0
Gilwoo Lee <gilwoo301@gmail.com>
2019-08-26 10:20:21 -0700

    Increase penalty radius

--- brl_gym/envs/mujoco/point_mass.py
+++ brl_gym/envs/mujoco/point_mass.py
@@ -37,13 +37,13 @@ class PointMassEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         # Distance to the other target
         other_target = 1 if self.target == 0 else 0
         dist_to_other = np.linalg.norm(GOAL_POSE[other_target] - agent_pos[:2])
- 
+
         reward = -np.linalg.norm(a) * 0.1
         done = False
         if dist < 0.2:
             reward = 500.0 # bonus for being very close
             done = True
-        if dist_to_other < 0.2:
+        if dist_to_other < 0.3:
             reward = -500 # Penalty for getting close to the other target
             done = True
 

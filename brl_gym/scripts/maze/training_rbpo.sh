db1fd6a12b89c9a761667c3421e5312fda11ee17
Gilwoo Lee <gilwoo301@gmail.com>
2019-09-11 10:47:37 -0700

    Fix script

--- brl_gym/scripts/maze/training_rbpo.sh
+++ brl_gym/scripts/maze/training_rbpo.sh
@@ -1,4 +1,4 @@
-OPENAI_LOGDIR=~/models/maze-high-sensing-cost/rbpo OPENAI_LOG_FORMAT=tensorboard python -m brl_baselines.run --alg=bppo2_expert --env=Maze-entropy-only-v0 --num_timesteps=5e8 --save_path=~/models/complete/maze_high-sensing-cost-rbpo --num_env=20 --save_interval=50  --gamma=1 --value_network=copy  --nminibatches=20
+OPENAI_LOGDIR=~/models/maze-high-sensing-cost/rbpo OPENAI_LOG_FORMAT=tensorboard python -m brl_baselines.run --alg=bppo2_expert --env=Maze-v0 --num_timesteps=5e8 --save_path=~/models/complete/maze_high-sensing-cost-rbpo --num_env=20 --save_interval=50  --gamma=1 --value_network=copy  --nminibatches=20
 
 
 #echo "Finished Training Maze for Entropy-only on instance 2" | ssmtp gilwoo301@gmail.com

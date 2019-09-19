3c9ab3ffed3741193bb9cfe0770e813a8f63e22e
Gilwoo Lee <gilwoo301@gmail.com>
2019-09-19 13:53:12 -0700

    Add alpha-1 scripts for doors

---
+++ brl_gym/scripts/doors/training_rbpo_no_ent_reward_alpha_1.sh
@@ -0,0 +1,6 @@
+#echo "Started Training Doors for RBPO on instance 3" | ssmtp gilwoo301@gmail.com
+OPENAI_LOGDIR=~/models/doors/rbpo_alpha_1 OPENAI_LOG_FORMAT=tensorboard python -m brl_baselines.run --alg=bppo2_expert --env=Door-no-entropy-v0 --num_timesteps=5e8 --save_path=~/models/complete/doors_rbpo_alpha_1 --num_env=20 --save_interval=50  --gamma=1 --value_network=copy  --nminibatches=20 --residual_weight=1.0
+
+#echo "Finished Training Doors for RBPO on instance 3" | ssmtp gilwoo301@gmail.com
+
+#sudo poweroff

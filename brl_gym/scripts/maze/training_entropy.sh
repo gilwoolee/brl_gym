380e4611ea46e8984dd7ea9c9ea4ee59fdc693e4
Gilwoo Lee <gilwoo301@gmail.com>
2019-09-20 12:24:08 -0700

    Add training scripts

--- brl_gym/scripts/maze/training_entropy.sh
+++
@@ -1,7 +0,0 @@
-echo "Started Training Maze for Entropy-onpy on instance 2" | ssmtp gilwoo301@gmail.com
-OPENAI_LOGDIR=~/models/maze/entropy_rbpo OPENAI_LOG_FORMAT=tensorboard python -m brl_baselines.run --alg=bppo2_expert --env=Maze-entropy-only-v0 --num_timesteps=5e8 --save_path=~/models/complete/maze_entropy_rbpo --num_env=20 --save_interval=50  --gamma=1 --value_network=copy  --nminibatches=20
-
-
-echo "Finished Training Maze for Entropy-only on instance 2" | ssmtp gilwoo301@gmail.com
-
-sudo poweroff

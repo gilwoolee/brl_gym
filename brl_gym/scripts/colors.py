bef174f865cc1d4427a66f94e36b98f350285d1d
Gilwoo Lee <gilwoo301@gmail.com>
2019-09-23 17:24:02 -0700

    Add color script

---
+++ brl_gym/scripts/colors.py
@@ -0,0 +1,14 @@
+
+random_color = [0.6, 0.6, 0.6]
+max_color = [0.4, 0.4, 0.4]
+expert_color = "#597223"
+brpo_color = "#e41a1c"
+upmle_color = "#9C72A3"
+bpo_color = "#577AA3"
+
+ent_reward_0 = brpo_color
+ent_reward_10 = "#4B7A4F"
+ent_reward_100 = "#795245"
+
+ensemble_color = "#504E75"
+none_color = "#8C7F70"
